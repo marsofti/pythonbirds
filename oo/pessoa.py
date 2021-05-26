dbe51df8603497ef958f649a57697c2e4fcebd92
Marcelo <marsofti@gmail.com>
2021-05-26 16:00:57 -0300

    Criada a classe Pessoa Criado método cumprimentar

--- oo/pessoa.py
+++ oo/pessoa.py
@@ -1,2 +1,15 @@
 class Pessoa:
-    pass
+    #metodo funcao pertence a classe
+    def cumprimentar(self):
+        return f'Olá {id(self)}'
+
+if __name__ == '__main__':
+    p = Pessoa()
+    print(Pessoa.cumprimentar(p))
+    print(id(p))
+    print(p.cumprimentar())
+
+
+
+
+
